fc27a4c5839db488b68cbeb50e196f594be7a31f
mohzib <mohzibansari343@gmail.com>
2026-02-06 19:49:04 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
+import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
-import os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")  # Railway variable se token le raha hai
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("✅ Bot is alive!")
@@ -10,7 +10,6 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 def main():
     app = ApplicationBuilder().token(BOT_TOKEN).build()
     app.add_handler(CommandHandler("start", start))
-    print("Bot started")
     app.run_polling()
 
 if __name__ == "__main__":
